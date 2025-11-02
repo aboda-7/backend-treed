a61cf671c76976984ba10eb99a9482149ce126f2
fantaMAK <183607087+fantaMAK@users.noreply.github.com>
2025-11-02 13:55:16 +0200

    test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import firebase_admin
 from firebase_admin import credentials, firestore, auth as firebase_auth
 from dotenv import load_dotenv
 import os, json, pathlib
-
+#kashta here
 # ----------------------------------------------------------------
 #  --- LOAD ENV ---
 # ----------------------------------------------------------------
